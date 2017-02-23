d1a3c51b86a9f3bbee1782ef25db9caaadd72dad
eirikhanssen <eirikhanssen79@gmail.com>
2017-02-23 13:37:17 +0100

    rename files. enqueue admin js

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,9 +16,11 @@
 			return;
 		}
 		
-		wp_register_style('fsnat_admin', get_template_directory_uri() . '/css/fsnat.admin.css', array(), '1.0.0', 'all');
+		wp_register_style('fsnat-admin', get_template_directory_uri() . '/css/fsnat.admin.css', array(), '1.0.0', 'all');
+		wp_enqueue_style('fsnat-admin');
 
-		wp_enqueue_style('fsnat_admin');
+		wp_register_script('fsnat-admin-script', get_template_directory_uri() . '/js/fsnat.admin.js', array('jquery'), '1.0.0', true);
+		wp_enqueue_script('fsnat-admin-script');		
 	}
 
 add_action('admin_enqueue_scripts', 'fsnat_load_admin_scripts');
